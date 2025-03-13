73afc5776c89a02dc0fe814947fe0a0a41992072
anurag.sharma@sourcefuse.com <anuragsharmaSF>
2025-03-13 16:34:32 +0530

    m

---
+++ Terraform/vpc/main.tf
@@ -0,0 +1,17 @@
+module "vpc_network" {
+  source = "../../modules/vpc"
+
+  aws_vpc_cidr         = "10.0.0.0/16"
+  vpc_name             = "my-vpc"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnet_azs    = ["ap-south-1a", "ap-south-1b"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnet_azs   = ["ap-south-1a", "ap-south-1b"]
+  igw_name             = "my-igw"
+  nat_eip_name         = "Nat-eip"
+  public_rt_name       = "my-public-rt"
+  private_rt_name      = "my-private-rt"
+  nat_gateway_name     = "my-nat-gateway"
+}
+
+
