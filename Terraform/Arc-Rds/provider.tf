c20f5c8d9f9f000aa126616ab1d005214e37ac28
anuragsharmaSF <anurag.sharma@sourcefuse.com>
2025-04-04 11:32:57 +0530

    Arc-rds module completed

---
+++ Terraform/Arc-Rds/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.3, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0, < 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1" # Change to your preferred region
+}
